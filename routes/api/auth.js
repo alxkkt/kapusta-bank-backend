f51751285bd41bc00f0807bcedcae96d278ddd82
Kasianenko Oleksandr <sanekkas@gmail.com>
2022-08-08 18:49:10 +0300

    delete unusable files

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -67,33 +67,4 @@ router.patch(
   ctrlWrapper(ctrl.balanceUpdate)
 );
 
-// const avatarDir = path.join(__dirname, "../../", "public", "avatars");
-
-// router.patch(
-//   "/avatar",
-//   authorize,
-//   upload.single("avatar"),
-//   async (req, res, next) => {
-//     try {
-//       const { _id } = req.user;
-//       const { path: tempDir, originalname } = req.file;
-
-//       const [ext] = originalname.split(".").reverse();
-//       const avatarName = `${_id}.${ext}`;
-//       const avatarPath = path.join(avatarDir, avatarName);
-
-//       await fs.rename(tempDir, avatarPath);
-//       const avatarURL = path.join("/avatars", avatarName);
-//       await User.findByIdAndUpdate(_id, { avatarURL });
-
-//       res.json({ avatarURL });
-//     } catch (error) {
-//       await fs.unlink(req.file.path);
-//       next(error);
-//     }
-//   }
-// );
-
-module.exports = router;
-
 module.exports = router;
